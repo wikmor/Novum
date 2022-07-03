32f20d1d9576d7cd19540c2e259248b73e222017
wikmor <100677740+wikmor@users.noreply.github.com>
2022-07-03 17:29:56 +0200

    Add '$100 calculator' lesson

--- src/me/wikmor/novum/Novum.java
+++ src/me/wikmor/novum/Novum.java
@@ -1,5 +1,7 @@
 package me.wikmor.novum;
 
+import java.util.Scanner;
+
 import me.wikmor.novum.house.House;
 import me.wikmor.novum.person.Employee;
 import me.wikmor.novum.person.EntityRegister;
@@ -10,7 +12,22 @@ import me.wikmor.novum.water.WaterBottle;
 
 public class Novum {
 
+	private static final int LEFT_HAND_NUMBER = 0;
+	private static final int RIGHT_HAND_NUMBER = 1;
+	private static final int OPERATOR = 2;
+
 	public static void main(String[] args) {
+		Novum novum = new Novum();
+
+		try {
+			novum.loop();
+
+		} catch (Throwable t) {
+			System.out.println("Oops, an error occured: " + t.getMessage());
+
+			t.printStackTrace();
+		}
+
 		Person person = new Person();
 		System.out.println("person = " + person.getName() + ", age: " + person.getAge());
 
@@ -78,5 +95,65 @@ public class Novum {
 		//	System.out.println("Make: " + );
 
 		System.out.println("Registered water bottles: " + BottleRegister.getRegisteredBottles());
+	}
+
+	private void loop() {
+
+		Scanner scanner = new Scanner(System.in);
+		String message = "";
+
+		int mode = LEFT_HAND_NUMBER;
+
+		double leftHandNumber = 0, rightHandNumber = 0;
+
+		while (!message.equals("exit")) {
+
+			if (mode > OPERATOR)
+				mode = 0;
+
+			if (mode == LEFT_HAND_NUMBER)
+				System.out.println("Please type the first number.");
+
+			else if (mode == RIGHT_HAND_NUMBER)
+				System.out.println("Please type the second number.");
+
+			else
+				System.out.println("Please type the operator: + - / * %");
+
+			message = scanner.nextLine();
+
+			if (mode == LEFT_HAND_NUMBER)
+				leftHandNumber = Double.parseDouble(message);
+
+			else if (mode == RIGHT_HAND_NUMBER)
+				rightHandNumber = Double.parseDouble(message);
+
+			else {
+				if ("+".equals(message))
+					System.out.println(leftHandNumber + " + " + rightHandNumber + " = " + (leftHandNumber + rightHandNumber));
+
+				else if ("-".equals(message))
+					System.out.println(leftHandNumber + " - " + rightHandNumber + " = " + (leftHandNumber - rightHandNumber));
+
+				else if ("/".equals(message))
+					System.out.println(leftHandNumber + " / " + rightHandNumber + " = " + leftHandNumber / rightHandNumber);
+
+				else if ("*".equals(message))
+					System.out.println(leftHandNumber + " * " + rightHandNumber + " = " + leftHandNumber * rightHandNumber);
+
+				else if ("%".equals(message))
+					System.out.println(leftHandNumber + " % " + rightHandNumber + " = " + leftHandNumber % rightHandNumber);
+
+				else {
+					System.out.println("Invalid operator: " + message);
+
+					continue;
+				}
+			}
+
+			mode++;
+		}
+
+		System.out.println("The program has quit. Good luck!");
 	}
 }
